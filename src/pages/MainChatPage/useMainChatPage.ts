40270b1e4404e9e4fc56216d3c8b50c649abb455
Srinivas G <Srinivas.G@apexon.com>
2026-02-18 12:47:31 +0530

    fetched replaced with axios

--- src/pages/MainChatPage/useMainChatPage.ts
+++ src/pages/MainChatPage/useMainChatPage.ts
@@ -8,13 +8,25 @@ import { useAppToast } from "../../Context/AppGlobalToast";
 import api from "../../api";
 import { useGlobalContext } from "../../Context/AppGlobalData";
 import { generateSessionId, getNewChatSessionId } from "../../utility";
+// import { useSelector, useDispatch } from "react-redux";
+// import { type AppDispatch, type RootState } from "../../react-redux/chatStore";
+// import { fetchUser } from "../../react-redux/chatSlice";
+// import { updateName, updateAge } from "../../react-redux/chatSlice";
 
 // import { ApiChatTempData } from "./ApiResponse";
 
 const getChatDetails = () => {
+  // const dispatch = useDispatch<AppDispatch>();
+  // const profile = useSelector((state: RootState) => state.user.profile);
+  // dispatch(updateName("Alice"));
+  // dispatch(updateAge(25));
+
   const [enteredChat, setEnteredChat] = useState<string>(
     "For patients dropping out of our diabetes studies, analyze the patient journey to identify intervention points. Focus on visit burden and digital endpoint acceptance",
   );
+
+  // console.log("Updated store state:", profile); // may show old value until re-render console.log("Direct store access:", store.getState().user.profile);
+
   const { setAppGlobalData, appGlobalData, clearChatSession } =
     useGlobalContext();
   const { addNewToast } = useAppToast();
@@ -37,9 +49,46 @@ const getChatDetails = () => {
     });
   };
 
+  // async function customInvokeAgent(prompt: any, jwtToken: string) {
+  //   // Your agent ARN
+  //   let arnSessionId = localStorage.getItem("arn_session_id")?.toString();
+  //   const arn = import.meta.env.VITE_AGENT_ARN;
+
+  //   // Build the correct URL
+  //   const url = `https://bedrock-agentcore.us-east-1.amazonaws.com/runtimes/${encodeURIComponent(
+  //     arn,
+  //   )}/invocations`;
+
+  //   const body = {
+  //     prompt,
+  //   };
+
+  //   const response = await fetch(url, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: `Bearer ${jwtToken}`,
+  //       "x-amzn-bedrock-agentcore-runtime-session-id": arnSessionId || "",
+  //     },
+
+  //     body: JSON.stringify(body),
+  //   });
+
+  //   if (!response.ok) {
+  //     const text = await response.text();
+  //     addNewToast({
+  //       type: "ERROR",
+  //       content: `Error invoking agent: ${response.status} ${text}`,
+  //     });
+  //     throw new Error(`Error invoking agent: ${response.status} ${text}`);
+  //   }
+
+  //   return await response.json();
+  // }
+
   async function customInvokeAgent(prompt: any, jwtToken: string) {
     // Your agent ARN
-    let arnSessionId = localStorage.getItem("arn_session_id")?.toString();
+    const arnSessionId = localStorage.getItem("arn_session_id")?.toString();
     const arn = import.meta.env.VITE_AGENT_ARN;
 
     // Build the correct URL
@@ -47,35 +96,42 @@ const getChatDetails = () => {
       arn,
     )}/invocations`;
 
-    const body = {
+    let payload = {
       prompt,
+      // runtimeSessionId: arnSessionId,
     };
-    console.log(
-      "appGlobalData?.chatSessionDetails?.currChatSessionId : " +
-        JSON.stringify(appGlobalData?.chatSessionDetails),
-    );
 
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${jwtToken}`,
-        "x-amzn-bedrock-agentcore-runtime-session-id": arnSessionId || "",
-      },
+    const params: Record<string, string> = {};
+    params.qualifier = "DEFAULT";
 
-      body: JSON.stringify(body),
-    });
+    try {
+      const response = await api.post(
+        "https://c2gu865khj.execute-api.us-east-1.amazonaws.com/cda-supervisor-invoke",
+        payload, // Axios automatically stringifies JSON
+        // {
+          // headers: {
+          //   "Content-Type": "application/json",
+            // Authorization: `Bearer ${jwtToken}`,
+            // ...(arnSessionId && {
+            //   "x-amzn-bedrock-agentcore-runtime-session-id": arnSessionId,
+            // }),
+          // },
+          // params,
+        // },
+      );
+      console.log("Agent response:", response);
+      return response.data;
+    } catch (error: any) {
+      const status = error.response?.status;
+      const text = error.response?.data || error.message;
 
-    if (!response.ok) {
-      const text = await response.text();
       addNewToast({
         type: "ERROR",
-        content: `Error invoking agent: ${response.status} ${text}`,
+        content: `Error invoking agent: ${status ?? ""} ${text}`,
       });
-      throw new Error(`Error invoking agent: ${response.status} ${text}`);
-    }
 
-    return await response.json();
+      throw new Error(`Error invoking agent: ${status ?? ""} ${text}`);
+    }
   }
 
   const checkForSessionIdAndProceed = async () => {
@@ -109,23 +165,27 @@ const getChatDetails = () => {
       };
 
       //this api is for creating session-ids
-      await api
-        .post(import.meta.env.VITE_CHAT_URL, newChatPayload)
-        .then((res) => {
-          localStorage.setItem("arn_session_id", arnSessionId);
-          setAppGlobalData((prevData: any) => ({
+      api.post(import.meta.env.VITE_CHAT_URL, newChatPayload).then((res) => {
+        localStorage.setItem("arn_session_id", arnSessionId);
+        setAppGlobalData((prevData: any) => {
+          return {
             ...prevData,
             userDetails: { userId: res?.data?.session?.user_id },
             chatSessionDetails: {
               currChatId: res?.data?.session?.session_id,
               currChatSessionId: arnSessionId,
+              lastQuestion: enteredChat,
             },
-          }));
-          setTimeout(() => {
-            onUserChatEnter(newChatSessionId);
-          }, 250);
+          };
         });
+        setTimeout(() => {
+          onUserChatEnter(newChatSessionId);
+        }, 250);
+      });
     } else {
+      if (!localStorage.getItem("user_name")) {
+        localStorage.setItem("arn_session_id", arnSessionId);
+      }
       onUserChatEnter(newChatSessionId);
     }
   };
@@ -133,14 +193,7 @@ const getChatDetails = () => {
   const onUserChatEnter = async (newChatSessionId: any) => {
     setWaitingForResponse(true);
     let promptRes: any = "";
-
-    setAppGlobalData((prevData: any) => ({
-      ...prevData,
-      chatSessionDetails: {
-        ...prevData?.chatSessionDetails,
-        lastQuestion: enteredChat,
-      },
-    }));
+    console.log(JSON.stringify(appGlobalData?.chatSessionDetails));
 
     setTimeout(async () => {
       try {
@@ -148,6 +201,9 @@ const getChatDetails = () => {
           enteredChat,
           localStorage.getItem("access_token") || "",
         );
+
+        console.log(appGlobalData);
+
         setAppGlobalData((prevData: any) => ({
           ...prevData,
           currentChatDetails: [
